c03e4bd0b5e60695387fb6e572294c508a398aa3
a0527105208 <a0527105208@gmail.com>
2026-03-06 03:23:13 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,1 @@
-const express = require("express");
-const app = express();
-
-app.get("/ivr-api",(req,res)=>{
-    console.log(req.query);
-
-    res.send(
-        "read=t-המערכת פועלת הקישו 1 לבדיקה=digits,1,1,1,7,yes,no&action=test"
-    );
-});
-
-app.listen(3000,()=>{
-    console.log("server started");
-});
+res.send("say=t-המערכת פועלת&go_to=https://yemot-rides.onrender.com/ivr-api?action=main");
